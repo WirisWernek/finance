ca113dd024399ab5cd372fc10b0b22e065d9318f
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-09-21 22:34:07 -0300

    feat: melhora apresentação dos cards de balanço

--- src/app/aplication/aplication.module.ts
+++ src/app/aplication/aplication.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { CardComponent } from './components/card/card.component';
+import { BalanceBarComponent } from './components/balance-bar/balance-bar.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent, CardComponent],
+  declarations: [HomeComponent, CardComponent, BalanceBarComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class AplicationModule {}
---
+++ src/app/aplication/components/balance-bar/balance-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-bar',
+  templateUrl: './balance-bar.component.html',
+  styleUrls: ['./balance-bar.component.scss']
+})
+export class BalanceBarComponent {
+
+}
